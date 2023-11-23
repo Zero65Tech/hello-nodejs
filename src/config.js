819b751bc6ea6afcc538879af03b2859f87f99be
Prashant Gupta <antshpra@gmail.com>
2023-11-23 23:02:33 +0530

    Integrating eslint, jest & public config

--- src/config.js
+++
@@ -1,9 +0,0 @@
-try {
-
-  module.exports = require('@zero65/config');
-
-} catch (e) {
-
-  exports.message = 'Hello World !';
-
-}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app     = express();
 
-const config = require('./config');
+const config = require('@zero65tech/config');
 
 
 
