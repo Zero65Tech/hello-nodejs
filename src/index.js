11d238a6b5c2477428f939d5621f8a5e4039b711
rahul-zer65 <rahul@zero65.in>
2023-11-24 19:34:41 +0530

    Revert "implement routing and Async await for testing"
    This reverts commit 25c8479d98dd993872be178acb6a3c7212cc17fa.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app     = express();
-const route   = require('./routes/route')
 
-app.use('/', route)
+const config = require('@zero65tech/config');
+
+
+
+app.get('/', async (req, res) => {
+  res.status(200).send(config.message);
+});
+
+app.get('/_env', async (req, res) => {
+  res.send(process.env);
+});
+
 
 app.listen(process.env.PORT || 8080, console.log(`index: Server is up and listening at ${ process.env.PORT || 8080 } port.`));
+
+module.exports = {app}
