07cc2023eaebc3d342850e9dc9431cd0899476d3
Prashant Gupta <antshpra@gmail.com>
2023-11-26 12:46:50 +0530

    server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
+process.env.STAGE = process.env.STAGE || 'alpha';
+
 const app  = require('./src/index.js');
 const port = process.env.PORT || 8080;
-
 app.listen(port, console.log(`Server is up and listening at ${ port } port.`));
