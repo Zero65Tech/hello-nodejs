4b107c428b42d355731c99fb0b773580c714c744
Prashant Gupta <antshpra@gmail.com>
2023-11-26 10:58:37 +0530

    Test Suite
    * testing start
    
    * implement routing and Async await for testing
    
    * Revert "implement routing and Async await for testing"
    
    This reverts commit 25c8479d98dd993872be178acb6a3c7212cc17fa.
    
    * remove env.json
    
    * implement routing
    
    * Test Suite
    
    ---------
    
    Co-authored-by: rahul-zer65 <rahul@zero65.in>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app     = express();
 
-const config = require('@zero65tech/config');
-
-
+const config  = require('@zero65tech/config');
 
 app.get('/', async (req, res) => {
   res.send(config.message);
@@ -13,6 +11,4 @@ app.get('/_env', async (req, res) => {
   res.send(process.env);
 });
 
-
-
-app.listen(process.env.PORT || 8080, console.log(`index: Server is up and listening at ${ process.env.PORT || 8080 } port.`));
+module.exports = app;
---
+++ test/index.test.js
@@ -0,0 +1,29 @@
+const request = require('supertest');
+const app     = require('../src/index');
+
+
+
+test('/', async () => {
+
+  const response = await request(app).get('/');
+
+  expect(response.status).toBe(200);
+  expect(response.headers['content-type']).toMatch(/^text\/html;/);
+  
+  expect(response.text).toBe('Hello NodeJs !');
+
+});
+
+test('/_env', async () => {
+
+  const response = await request(app).get('/_env');
+
+  expect(response.status).toBe(200);
+  expect(response.headers['content-type']).toMatch(/^application\/json;/);
+
+  const env = JSON.parse(response.text);
+
+  expect(env.npm_package_name).toBe('hello-nodejs');
+  expect(env.STAGE).toMatch(/^(alpha|beta|gamma|prod)$/);
+
+});
