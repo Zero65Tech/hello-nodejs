6a93f07ca18f9de564d11f409a9acad1d0c08c50
Prashant Gupta <antshpra@gmail.com>
2023-12-02 00:58:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-const Config = require('@zero65/config');
 
-require('./app/index.js')
-    .listen(
-        process.env.PORT,
-        console.log(`\n\nServer (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.\n\n`));
+require('./app').listen(
+    process.env.PORT,
+    console.log(`\n\nServer (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.\n\n`));
