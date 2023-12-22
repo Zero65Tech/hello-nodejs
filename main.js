52533f07da89fb553681d7142b841a3513621129
Prashant Gupta <antshpra@gmail.com>
2023-12-22 07:59:31 +0530

    gcp init

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+(async () => {
 
-require('./app').listen(
-    process.env.PORT=3000,
-    console.log(`\n\nServer (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.\n\n`));
+  const Config = require('@zero65/config');
+  
+  const GCP = require('@zero65tech/google-cloud-platform');
+  await GCP.init(Config['@zero65tech']['google-cloud-platform']);
+
+  require('./app').listen(
+      process.env.PORT,
+      console.log(`\n\nServer (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.\n\n`));
+
+})();
