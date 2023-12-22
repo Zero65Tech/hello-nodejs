e730d725620ef32b85b90e3bd5871300fa048e2d
Prashant Gupta <antshpra@gmail.com>
2023-12-22 09:16:46 +0530

    api port

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 (async () => {
 
+  const Config = require('@zero65/config'); // Sets PORT to 8080
+
   const GCP = require('@zero65tech/google-cloud-platform');
   await GCP.init({
     firestore: {
       project: 'zero65-test',
-      collections: collections[ 'HELLO_DOCUMENTS' ]
+      collections: [ 'HELLO_DOCUMENTS' ]
     }
   });
 
   require('./app').listen(
-      process.env.PORT || 8080,
+      process.env.PORT,
       console.log(`\n\nServer (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.\n\n`));
 
 })();
