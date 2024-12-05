b14260c71d4d30d112698798bb324db927fa4414
Prashant Gupta <antshpra@gmail.com>
2024-12-05 17:37:22 +0000

    clean-up

---
+++ src/index.js
@@ -0,0 +1,16 @@
+(async () => {
+
+  process.env.STAGE = process.env.STAGE || 'alpha';
+  process.env.PORT  = process.env.PORT  || 8080;
+
+  const Firestore = require('@zero65tech/firestore');
+  await Firestore.init({
+    project: 'zero65-test',
+    collections: [ 'HELLO_DOCUMENTS' ]
+  });
+
+  require('../app').listen(
+      process.env.PORT,
+      console.log(`\n\nServer (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.\n\n`));
+
+})();
