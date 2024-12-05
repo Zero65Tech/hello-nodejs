a2c69daeaa47878ebba62a991a6aa0198ded2f12
Prashant Gupta <antshpra@gmail.com>
2024-12-05 17:59:05 +0000

    refactoring

--- cron/index.js
+++
@@ -1,7 +0,0 @@
-const Log = new (require('@zero65tech/log'))('cron');
-
-module.exports = () => {
-
-  Log.info('logic goes here ...', 1, 2);
-
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Data = require('../../data');
+const Data = require('../services');
 
 
 
