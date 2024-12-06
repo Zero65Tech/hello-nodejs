8b4afec52ca4a4bd627dbed8d4deabf5ae89aeed
Prashant Gupta <antshpra@gmail.com>
2024-12-06 11:22:56 +0000

    devcontainer

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,12 +2,13 @@
 
 echo "@zero65:registry=https://asia-south1-npm.pkg.dev/zero65/npm/" > ~/.npmrc
 
-echo "$GOOGLE_APPLICATION_CREDENTIALS" > ~/service-account.json
-export GOOGLE_APPLICATION_CREDENTIALS="~/service-account.json"
+echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > ~/service-account.json
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/service-account.json"
+echo "$GOOGLE_APPLICATION_CREDENTIALS"
 
 npm install -g google-artifactregistry-auth nodemon
 
 npx google-artifactregistry-auth
 npm install
 
-echo "DONE !" > ./.devcontainer/status.txt
+echo "DONE !" > ./.devcontainer/DONE
