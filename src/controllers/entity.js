85508c2d4c4d8bb8fcb68ca9ebfb8f6334ab8c9f
Prashant Gupta <antshpra@gmail.com>
2024-12-07 18:44:44 +0000

    Fix

--- src/controllers/entity.js
+++ src/controllers/entity.js
@@ -1,32 +1,32 @@
-const Data = require('../models/entity');
+const entityService = require('../services/entity');
 
 
 
 exports.list = async (req, res) => {
-  const list = await Data.list();
+  const list = await entityService.list();
   res.send(list);
 };
 
 exports.post = async (req, res) => {
   let data = req.body;
-  data = await Data.add(data);
+  data = await entityService.add(data);
   res.send(data);
 };
 
 exports.put = async (req, res) => {
   const { id, ...updates } = req.body;
-  await Data.update(id, updates);
+  await entityService.update(id, updates);
   res.sendStatus(204);
 };
 
 exports.patch = async (req, res) => {
   const { id, ...updates } = req.body;
-  await Data.update(id, updates);
+  await entityService.update(id, updates);
   res.sendStatus(204);
 };
 
 exports.delete = async (req, res) => {
   const { id } = req.body;
-  await Data.purge(id);
+  await entityService.purge(id);
   res.sendStatus(204);
 };
