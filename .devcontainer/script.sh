c4f5889a539f302ff957859c6cc5e77389cc26dd
Prashant Gupta <antshpra@gmail.com>
2024-12-08 10:45:40 +0000

    Fix in devcontainer script

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -2,13 +2,12 @@
 
 echo "@zero65:registry=https://asia-south1-npm.pkg.dev/zero65/npm/" > ~/.npmrc
 
+mkdir -p ~/.config/gcloud
 if [ -n "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" ]; then
-    echo "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" | base64 --decode > ~/service-account.json
+    echo "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" | base64 --decode > ~/.config/gcloud/application_default_credentials.json
 elif [ -n "$GOOGLE_APPLICATION_CREDENTIALS_JSON" ]; then
-    echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > ~/service-account.json
+    echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > ~/.config/gcloud/application_default_credentials.json
 fi
-
-export GOOGLE_APPLICATION_CREDENTIALS="$HOME/service-account.json"
 
 npm install -g google-artifactregistry-auth typescript nodemon
 
