326321a4584eb1894a3c94b910687cbc77186cb3
Prashant Gupta <antshpra@gmail.com>
2024-12-09 08:46:53 +0530

    Update script.sh

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -9,6 +9,6 @@ elif [ -n "$GOOGLE_APPLICATION_CREDENTIALS_JSON" ]; then
     echo "$GOOGLE_APPLICATION_CREDENTIALS_JSON" > ~/.config/gcloud/application_default_credentials.json
 fi
 
-npm install -g google-artifactregistry-auth typescript nodemon
+npm install -g google-artifactregistry-auth nodemon
 
 npx google-artifactregistry-auth && npm install && npm run dev
