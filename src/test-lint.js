c727edfe8358ed0ee9dc00bf374aa5548a7dd079
navneet singh <rooks.ns@gmail.com>
2024-12-09 19:32:18 +0530

    testing new linter

---
+++ src/test-lint.js
@@ -0,0 +1,13 @@
+// test-lint.js
+
+// Intentionally using double quotes if your ESLint prefers single quotes:
+const greeting = "Hello from ESLint test!";
+
+// Unused variable (if your ESLint is configured to complain about it):
+const unusedVariable = 42;
+
+// Extra spaces at the end of this line:  
+console.log( greeting );    
+
+// Missing semicolon if your ESLint enforces semicolons
+console.log("Lint me!");
