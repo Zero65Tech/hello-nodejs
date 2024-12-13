d3348820fb7ce62b447e2532b6de4479a56930ab
rooks-ns <rooks.ns@gmail.com>
2024-12-13 20:46:08 +0000

    testing lint workflow on direct push to master branch

--- src/models/entity.js
+++ src/models/entity.js
@@ -1,12 +1,12 @@
 const Joi = require('joi');
 
 exports.add = Joi.object({
-  name: Joi.string().required(),
-  info: Joi.string().required(),
-  count: Joi.number().integer().min(0).required(),
-  timestamp: Joi.object({
-    create: Joi.date().iso().required(),
-    update: Joi.date().iso().required()
+    name: Joi.string().required(),
+      info: Joi.string().required(),
+          count: Joi.number().integer().min(0).required(),
+              timestamp: Joi.object({
+                create: Joi.date().iso().required(),
+                  update: Joi.date().iso().required()
   }).required()
 });
 
