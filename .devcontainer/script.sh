2f257d1dc7fc4137fe2719eeea103ee8314f9027
Prashant Gupta <antshpra@gmail.com>
2024-12-22 12:06:03 +0530

    Update script.sh

--- .devcontainer/script.sh
+++ .devcontainer/script.sh
@@ -4,7 +4,7 @@ if [ -n "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" ]; then
     echo "@zero65:registry=https://asia-south1-npm.pkg.dev/zero65/npm/" > ~/.npmrc
     mkdir -p ~/.config/gcloud
     echo "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" | base64 --decode > ~/.config/gcloud/application_default_credentials.json
-    npm install -g google-artifactregistry-auth nodemon
+    npm install -g google-artifactregistry-auth
     npx google-artifactregistry-auth
 fi
 
